199725635246d531ab3c06d397eae9a0bcea27b3
Elizavet-8 <70907348+Elizavet-8@users.noreply.github.com>
2021-10-30 18:40:13 +0300

    comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-forms';
+export class AppComponent implements OnInit {
+
+  exform: FormGroup;
+
+  phoneMask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
+
+  ngOnInit() {
+    this.exform = new FormGroup({
+      'name': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'phone': new FormControl(
+        null,
+        [
+          Validators.required,
+          Validators.pattern('^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$')
+        ]),
+      'password': new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      'confimPassword': new FormControl('', Validators.required)
+    });
+  }
+
+  clicksub() {
+    console.log(this.exform.value);
+    this.exform.reset();
+    alert('Всё ок!')
+  }
+  get name() {
+    return this.exform.get('name');
+  }
+  get email() {
+    return this.exform.get('email');
+  }
+  get phone() {
+    return this.exform.get('phone');
+  }
+  get password() {
+    return this.exform.get('password');
+  }
+
 }
